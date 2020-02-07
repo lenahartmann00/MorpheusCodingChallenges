a0c0190e5dba2bcad70eba97cfe905f91212d256
Lena Hartmann <lena.hartmann00@googlemail.com>
2020-02-07 22:12:58 +0100

    rewrite java doc

--- src/main/java/com/lenahartmann00/morpheus_challenges/logic/gson/JsonModel.java
+++ src/main/java/com/lenahartmann00/morpheus_challenges/logic/gson/JsonModel.java
@@ -1,11 +1,12 @@
 package com.lenahartmann00.morpheus_challenges.logic.gson;
 
 /**
- * This interface should be implemented as a json model class. It is used to highlight that in
- * {@link com.lenahartmann00.morpheus_challenges.logic.challenge.AbstractChallenge} the generic
- * types have to be a json model which are used by {@link com.google.gson.Gson}.<br> Note: The
- * variable names are used to match the attribute with the specific json object. So the attribute
- * names cannot differ from the json names.
+ * This interface is used to register a new json model class to {@link
+ * com.lenahartmann00.morpheus_challenges.logic.challenge.AbstractChallenge}. Such model classes are
+ * used to by {@link com.google.gson.Gson} when sending http requests.<br> Note: The variable names
+ * are used to match the attribute with the specific json object. So the attribute names cannot
+ * differ from the json names unless a own deserializer {@link com.google.gson.JsonDeserializer} is
+ * implemented.
  */
 public interface JsonModel {
 
